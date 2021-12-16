cbe5bcb3991c1d0a69d3c2c5eafc5f5e867b8dbf
ChrisRolling <chrisrolling27@gmail.com>
2021-12-16 09:26:54 -0800

    pizza start

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <div>App!</div>
-)
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+
+  //filter method
+
+  render() {
+    return (
+      <div>
+          Hello app here
+      </div>
+    )
+  }
+
+}
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('app'))
